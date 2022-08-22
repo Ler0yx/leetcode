f7b9ed4bf6cca8b00e944c2a4a06b27517b79336
leo <leo.opdenplatz@gmail.com>
2022-08-22 13:47:04 +0200

    added solution for problem 70: climbing stairs

---
+++ 70_climbing_stairs/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Calculates the amount of possibilities to climb a staircase with n amount of steps
+// if you can either take 1 or 2 steps at a time.
+func climbStairs(n int) int {
+	x := 1
+	y := 2
+
+	if n == 1 {
+		return x
+	} else if n == 2 {
+		return y
+	} else {
+		for i := 2; i < n; i++ {
+			tmp := x + y
+			x = y
+			y = tmp
+		}
+	}
+
+	return y
+}
+
+func main() {
+	fmt.Println(climbStairs(3))
+}
