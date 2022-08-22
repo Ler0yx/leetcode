9e8926bb7dd7e0670d11b41085fdc04537e9ba0b
leo <leo.opdenplatz@gmail.com>
2022-08-22 09:54:35 +0200

    First Problem solved after starting to work with GitHub

---
+++ fibonacci_number/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//Calculates the Fibonacci number (F(n) = F(n - 1) + F(n - 2), for n > 1.) for the given Input.
+func fib(n int) int {
+	x := 0
+	y := 1
+	sum := 0
+
+	if n == 0 {
+		return x
+	} else if n == 1 {
+		return y
+	} else {
+		for i := 1; i < n; i++ {
+			sum = x + y
+			x = y
+			y = sum
+		}
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(fib(2))
+}
