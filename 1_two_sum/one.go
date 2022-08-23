9f56a9e3abf3ae5d3cd67d71beedfc5da7265e40
leo <leo.opdenplatz@gmail.com>
2022-08-23 12:54:51 +0200

    added problem 1_two_sum

---
+++ 1_two_sum/one.go
@@ -0,0 +1,22 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+
+	var solution []int
+
+	//Loops through the array, used to get the first int.
+	for i := 0; i < len(nums); i++ {
+
+		//Also loops through the arry, used to get the second int. Starts 1 index after loop 1. 
+		for d := i + 1; d < len(nums); d++ {
+
+			//Builds a slice with the index of the first and second int, if the sum matches the target.
+			if nums[i]+nums[d] == target {
+				solution = append(solution, i, d)
+			}
+		}
+	}
+	return solution
+
+	//Problem: uses two for-loops which increases the runtime a lot.
+}
---
+++ 1_two_sum/optimal_solution.go
@@ -0,0 +1,30 @@
+package main
+
+func twoSum2(nums []int, target int) []int {
+
+	result := map[int]int{}
+
+	//Loops through the array, saving the index and value.
+	for i, v := range nums {
+
+		//Detracts the value from the target
+		remainder := target - v
+
+		//Creates a bool that turns true if the remainder is a key of result,
+        //meaning that the value at this index + the value at the index remainder = target
+		if _, ok := result[remainder]; ok {
+			return []int{result[remainder], i}
+
+        //If the bool is false, we create an entry with the value as the key and the index as the value
+		} else {
+			result[v] = i
+		}
+	}
+
+    //Just a placeholder return to satisy the compiler
+	return []int{}
+}
+
+func main() {
+
+}
