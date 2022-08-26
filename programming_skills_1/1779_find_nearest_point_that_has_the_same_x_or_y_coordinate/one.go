d31f49377512ce052505fbbb106a6e20ad8eefee
leo <leo.opdenplatz@gmail.com>
2022-08-26 10:18:37 +0200

    added problem 1779: find nearest point that has the same x or y coordinate

---
+++ programming_skills_1/1779_find_nearest_point_that_has_the_same_x_or_y_coordinate/one.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math"
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func nearestValidPoint(x int, y int, points [][]int) int {
+
+	smallestIdx := 0
+	manhattanDst := math.MaxInt64
+
+	for i := 0; i < len(points); i++ {
+		tmp := 0
+		if points[i][0] == x || points[i][1] == y {
+			tmp = abs(x-points[i][0]) + abs(y-points[i][1])
+			if tmp < manhattanDst {
+				manhattanDst = tmp
+				smallestIdx = i
+			}
+		}
+	}
+	if manhattanDst != math.MaxInt64 {
+		return smallestIdx
+	} else {
+		return -1
+	}
+}
+
+func main() {
+
+}
