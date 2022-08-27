e58470b9c06ed237b5da0a406c379da2ad85fcea
leo <leo.opdenplatz@gmail.com>
2022-08-27 16:48:52 +0200

    added problem 1502: can make arithmetic progression from sequence

---
+++ 1502_can_make_arithmetic_progression_from_sequence/one.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func canMakeArithmeticProgression(arr []int) bool {
+
+	var dst int
+
+	if len(arr) == 2 {
+		return true
+	}
+
+	sort.Ints(arr)
+
+	if arr[0] < 0 && arr[1] > 0 {
+		dst = abs(abs(arr[0]) + abs(arr[1]))
+	} else {
+		dst = abs(abs(arr[0]) - abs(arr[1]))
+	}
+
+	for i := 1; i < len(arr)-1; i++ {
+		if abs(abs(arr[i])-abs(arr[i+1])) != dst {
+
+			if arr[i] < 0 && arr[i+1] > 0 {
+				if abs(abs(arr[i])+abs(arr[i+1])) == dst {
+					fmt.Println("hier")
+					continue
+				}
+			}
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+
+	nums := []int{5, -5, 15}
+
+	fmt.Println(canMakeArithmeticProgression(nums))
+}
