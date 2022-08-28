4d2e4d58f696eb8fedf3d28cd7ca7a05682e2c11
leo <leo.opdenplatz@gmail.com>
2022-08-28 19:49:58 +0200

    added problem 202: happy number

---
+++ 202_happy_number/one.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func isHappy(n int) bool {
+
+	sum := 0
+
+	results := make(map[int]int)
+
+	nString := fmt.Sprintf("%d", n)
+	nSlice := strings.Split(nString, "")
+
+	for {
+		for i, v := range nSlice {
+			val, _ := strconv.Atoi(v)
+			sum += val * val
+			fmt.Printf("i: %d, sum: %d\n", i, sum)
+		}
+
+		if sum == 1 {
+			return true
+		} else if _, exists := results[sum]; exists {
+			return false
+		}
+		results[sum] = 1
+		fmt.Printf("%v\n", results)
+		nString = fmt.Sprintf("%d", sum)
+		nSlice = strings.Split(nString, "")
+		sum = 0
+	}
+
+}
+
+func main() {
+	fmt.Println(isHappy(2))
+}
