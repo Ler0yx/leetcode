a78c0ece106e2870dc313db0cac23bec34440883
leo <leo.opdenplatz@gmail.com>
2022-08-30 14:25:54 +0200

    added problem 575: distribute candies

---
+++ 575_distribute_candies/one.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func distributeCandies(candyType []int) int {
+
+	sort.Ints(candyType)
+	counter := 1
+
+	for i := 0; i < len(candyType)-1; i++ {
+		if candyType[i] != candyType[i+1] {
+			counter += 1
+		}
+	}
+
+	if len(candyType)/2 > counter {
+		return counter
+	} else {
+		return len(candyType) / 2
+	}
+}
+
+func main() {
+
+}
