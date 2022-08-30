dec8c8b81a9c7c3ff8a369ca60e4c48f155060fe
leo <leo.opdenplatz@gmail.com>
2022-08-30 15:10:57 +0200

    added problem 459: repeated substring pattern

---
+++ 459_repeated_substring_pattern/one.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func repeatedSubstringPattern(s string) bool {
+
+	var subString string
+
+	for i := 0; i < len(s)/2; i++ {
+		subString += string(s[i])
+		if len(s)%len(subString) == 0 {
+			if strings.Repeat(subString, len(s)/len(subString)) == s {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(repeatedSubstringPattern("ababab"))
+}
