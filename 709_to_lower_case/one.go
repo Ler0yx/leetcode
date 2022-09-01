51856236fb078ccb5482489acba1b10d0365089a
leo <leo.opdenplatz@gmail.com>
2022-09-01 10:42:51 +0200

    added problem 709: to lower case

---
+++ 709_to_lower_case/one.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func toLowerCase(s string) string {
+	byteSlice := []byte(s)
+	for i := 0; i < len(byteSlice); i++ {
+
+		if byteSlice[i] >= 65 && byteSlice[i] <= 90 {
+			byteSlice[i] += 32
+		}
+	}
+	return string(byteSlice)
+}
+
+func main() {
+	s1 := "heLLo"
+
+	fmt.Println(toLowerCase(s1))
+}
