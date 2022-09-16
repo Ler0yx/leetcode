31cf8e63b0f24987ad624d3e4b59c9b5682a4d04
leo <leo.opdenplatz@gmail.com>
2022-09-16 13:12:14 +0200

    added problem 234: palindrome linkes list

---
+++ 234_palindrome_linked_list/one.go
@@ -0,0 +1,34 @@
+package palindromeLinkedList
+
+func isPalindrome(head *ListNode) bool {
+
+	valMap := make(map[int]int)
+	turn := 0
+
+	for i := 0; head != nil; i++ {
+		valMap[i] = head.Val
+		head = head.Next
+
+		if i > 0 && valMap[i] == valMap[i-1] {
+			turn = i
+		}
+	}
+
+	if turn != len(valMap)/2 {
+		return false
+	}
+
+	if len(valMap) == 1 || len(valMap) == 2 && valMap[0] == valMap[1] {
+		return true
+	}
+
+	for i := turn; i < len(valMap); {
+		for j := turn - 1; j >= 0; j-- {
+			if valMap[i] != valMap[j] {
+				return false
+			}
+			i++
+		}
+	}
+	return true
+}
