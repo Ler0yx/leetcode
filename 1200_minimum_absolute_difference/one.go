bf72525343917daca92658f854f42b2d1623b01a
leo <leo.opdenplatz@gmail.com>
2022-09-20 16:13:14 +0200

    added problem 1200: minimum absolute difference

--- 1200_minimum_absolute_difference/one.go
+++ 1200_minimum_absolute_difference/one.go
@@ -1,24 +1,47 @@
 package minimumAbsoluteDifference
 
-func abs (x int) int {
-    if x < 0 {
-        return -x
-    }
-    return x
+import (
+	"math"
+	"sort"
+)
+
+// Too slow
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func minimumAbsDifference(arr []int) [][]int {
-    
-    var minDiff := math.MaxInt()
-    
-    for i:=0; i < len(arr)-1; i++ {
-        if abs(i) - abs(i+1) < minDiff {
-            minDiff = 
-        }
-    }
-}
 
-1: den niedrigsten absoluten Abstand finden
-2: aufsteigend sortieren
-3: abstaende mit minimum vergleichen
-4: paare sortieren
+	var minDiff = math.MaxInt
+	solution := make([][]int, 0)
+
+findminDiff:
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if i == j {
+				continue
+			} else if abs(arr[i]-arr[j]) < minDiff {
+				minDiff = abs(arr[i] - arr[j])
+			}
+			if minDiff == 1 {
+				break findminDiff
+			}
+		}
+	}
+
+	sort.Ints(arr)
+
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if abs(arr[i]-arr[j]) == minDiff {
+				tmp := []int{arr[i], arr[j]}
+				solution = append(solution, tmp)
+				break
+			}
+		}
+	}
+	return solution
+}
