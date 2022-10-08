3a534814630170feadab9219799168960be8c441
leo <leo.opdenplatz@gmail.com>
2022-10-08 19:59:41 +0200

    added problem 121: best time to buy and sell stock

---
+++ 121_best_time_to_buy_and_sell_stock/one.go
@@ -0,0 +1,15 @@
+package bestTimeToBuyAndSellSt
+
+func maxProfit(prices []int) int {
+
+	var max int
+
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j]-prices[i] > max {
+				max = prices[j] - prices[i]
+			}
+		}
+	}
+	return max
+}
