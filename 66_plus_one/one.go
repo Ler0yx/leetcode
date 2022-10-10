b044f9bdad297cbb3db326da25c7389c8586b08a
leo <leo.opdenplatz@gmail.com>
2022-10-10 17:21:51 +0200

    added problem 66: plus one

---
+++ 66_plus_one/one.go
@@ -0,0 +1,18 @@
+package plusOne
+
+func plusOne(digits []int) []int {
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i] += 1
+			break
+		}
+		if i == 0 {
+			digits[0] = 1
+			digits = append(digits, 0)
+		}
+	}
+	return digits
+}
