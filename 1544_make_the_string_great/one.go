fcb98a76dbb777dc627d3414463b0d992a3b064d
leo <leo.opdenplatz@gmail.com>
2023-01-11 15:09:38 +0100

    added problem 1544: make the string great

---
+++ 1544_make_the_string_great/one.go
@@ -0,0 +1,11 @@
+package makeTheStringGreat
+
+func makeGood(s string) string {
+    for i := 0; i < len(s)-1; i++ {
+        if s[i] == s[i+1]-32 || s[i]-32 == s[i+1] {
+            s = s[:i] + s[i+2:]
+            i = -1
+        }
+    }
+    return s
+}
